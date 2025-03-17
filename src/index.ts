8ab0a9ea76d21a7c3bffdedf23ce0952c97b9416
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-03-17 17:33:36 -0300

    new index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => {
   res.send('HERE IS DONE');
